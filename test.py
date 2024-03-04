9238bd52c4dcb310ed90acb504f0592b063f80f2
danx0r <danbmil99@gmail.com>
2024-03-03 19:39:20 -0800

    up difficulty

--- test.py
+++ test.py
@@ -8,11 +8,12 @@ A = 10
 V = 10
 
 env = gym.make("CartPole-v1", render_mode="human")
+env._max_episode_steps=1000
 observation, info = env.reset()
 
 resets = 0
 rewards = 0
-for _ in range(500):
+for _ in range(1000):
     pos, vel, ang, angv  = observation
     ctl = pos * P + vel * D + ang * A + angv * V
     #print (ctl)
@@ -20,7 +21,7 @@ for _ in range(500):
     observation, reward, terminated, truncated, info = env.step(action)
     rewards += reward
     #print (_, observation, reward, terminated, truncated, info)
-    if terminated or truncated:
+    if terminated or truncated or abs(observation[0]) > 0.1:
         print ("*************************RESET*************************")
         print (_, observation, "rewards:", rewards)
         observation, info = env.reset()
