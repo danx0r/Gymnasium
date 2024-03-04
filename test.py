830994fa7e0ea648511f6cfee68e1c9a49d2a8db
danx0r <danbmil99@gmail.com>
2024-03-03 18:29:57 -0800

    Some stability

--- test.py
+++ test.py
@@ -1,21 +1,23 @@
 import time
+import random
 import gymnasium as gym
 
-P = -1
+P = 9
 D = 0
+A = 10
+V = 5
 
 env = gym.make("CartPole-v1", render_mode="human")
 observation, info = env.reset()
 
-for _ in range(100):
-    pos = observation[0]
-    vel = observation[1]
-    ctl = pos * P + vel * D
-    action = 1 if ctl > 0 else 0
+for _ in range(500):
+    pos, vel, ang, angv  = observation
+    ctl = pos * P + vel * D + ang * A + angv * V
+    action = 1 if ctl + 0.5 > random.random() else 0
     observation, reward, terminated, truncated, info = env.step(action)
     print (_, observation, reward, terminated, truncated, info)
     if terminated or truncated:
         print ("*************************RESET*************************")
         observation, info = env.reset()
-    time.sleep(.05)
+    time.sleep(.015)
 env.close()
