9445852e7b1ebcc02db289cf9a7946078dc89b1c
danx0r <danbmil99@gmail.com>
2024-03-03 18:43:38 -0800

    works at 100 steps

--- test.py
+++ test.py
@@ -2,22 +2,27 @@ import time
 import random
 import gymnasium as gym
 
-P = 9
+P = 0
 D = 0
 A = 10
-V = 5
+V = 10
 
 env = gym.make("CartPole-v1", render_mode="human")
 observation, info = env.reset()
 
-for _ in range(500):
+resets = 0
+for _ in range(100):
     pos, vel, ang, angv  = observation
     ctl = pos * P + vel * D + ang * A + angv * V
+    #print (ctl)
     action = 1 if ctl + 0.5 > random.random() else 0
     observation, reward, terminated, truncated, info = env.step(action)
     print (_, observation, reward, terminated, truncated, info)
     if terminated or truncated:
         print ("*************************RESET*************************")
         observation, info = env.reset()
-    time.sleep(.015)
+        resets += 1
+    time.sleep(.005)
 env.close()
+
+print ("Golf score:", resets)
