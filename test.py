84d9403e4b1740813b571ee4520af3c0c070cfd0
danx0r <danbmil99@gmail.com>
2024-03-03 18:17:37 -0800

    unsable

--- test.py
+++ test.py
@@ -1,15 +1,21 @@
 import time
 import gymnasium as gym
 
+P = -1
+D = 0
+
 env = gym.make("CartPole-v1", render_mode="human")
 observation, info = env.reset()
 
 for _ in range(100):
-    time.sleep(.1)
-    action = 0 if observation[0] > 0 else 1  # agent policy that uses the observation and info
+    pos = observation[0]
+    vel = observation[1]
+    ctl = pos * P + vel * D
+    action = 1 if ctl > 0 else 0
     observation, reward, terminated, truncated, info = env.step(action)
     print (_, observation, reward, terminated, truncated, info)
     if terminated or truncated:
         print ("*************************RESET*************************")
         observation, info = env.reset()
+    time.sleep(.05)
 env.close()
