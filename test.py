aba98cb0471c8dfaa854f394a538548ce8a97270
danx0r <danbmil99@gmail.com>
2024-03-03 18:11:51 -0800

    bad pid

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
+import time
 import gymnasium as gym
-env = gym.make("LunarLander-v2", render_mode="human")
+
+env = gym.make("CartPole-v1", render_mode="human")
 observation, info = env.reset()
 
-for _ in range(1000):
-    action = env.action_space.sample()  # agent policy that uses the observation and info
+for _ in range(100):
+    time.sleep(.1)
+    action = 0 if observation[0] > 0 else 1  # agent policy that uses the observation and info
     observation, reward, terminated, truncated, info = env.step(action)
-
+    print (_, observation, reward, terminated, truncated, info)
     if terminated or truncated:
+        print ("*************************RESET*************************")
         observation, info = env.reset()
-
 env.close()
