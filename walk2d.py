4efdd1113e8165eab239b15808e4b657264ec581
danx0r <danbmil99@gmail.com>
2024-03-04 15:25:42 -0800

    fix model (was bird-like knee)

--- walk2d.py
+++ walk2d.py
@@ -2,36 +2,43 @@ import time
 import random
 import gymnasium as gym
 
-def controller(observation):
-    GAIN = 1.0
-    P = 5 * GAIN
-    D = 8 * GAIN
-    A = 16 * GAIN
-    V = 10 * GAIN
-    pos, vel, ang, angv  = observation
-    ctl = pos * P + vel * D + ang * A + angv * V
-    action = 1 if ctl + 0.5 > random.random() else 0
-    return action
+#
+# hip_r, knee_r, foot_r, hip_l, knee_l, foot_l
+# (mujoco calls them thigh, leg, foot)
+# right is tan, left is purple
+# hip: -1 extend (back bend) 1 contract (knee to nose)
+# knee: -1 contract (heel to butt; kneeling), 1 extend (locked knee)
+# foot: -1 extend (high heel/en pointe), 1 contract (walk on your heels)
+#
+def controller(obs, step):
+    print (f"torso height: {obs[0]}")
+    if step < 350:
+        act = [0, 1, 0, 0, 1, 0]
+    elif step < 700:
+        act = [-1, 1, 0, -1, 1, 0]
+    else:
+        act = [1, -1, 0, 1, -1, 0]
+    return act
 
 
-env = gym.make("Walker2d-v4", render_mode="human")
-env._max_episode_steps=1000
+env = gym.make("Walker2d-v4", render_mode="human", terminate_when_unhealthy=False)
+# env._max_episode_steps=1000
 observation, info = env.reset()
 
 resets = 0
 rewards = 0
 for _ in range(1000):
-    action = controller(observation)
+    action = controller(observation, _)
     observation, reward, terminated, truncated, info = env.step(action)
     rewards += reward
-    #print (_, observation, reward, terminated, truncated, info)
-    if terminated or truncated or abs(observation[0]) > 0.15:
+    print (_, terminated, truncated)
+    if terminated or truncated:
         print ("*************************RESET*************************")
         print (_, observation, "rewards:", rewards)
         observation, info = env.reset()
         resets += 1
         rewards = 0
-    #time.sleep(.005)
+    # time.sleep(.05)
 env.close()
 
 print ("Golf score:", resets)
