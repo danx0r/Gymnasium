ac151fdd7ee14c71d94f3ecc224298373857e22a
danx0r <danbmil99@gmail.com>
2024-03-12 14:36:04 -0700

    Args

--- pend.py
+++ pend.py
@@ -1,16 +1,18 @@
-import time
+import sys, time
 import random
 import gymnasium as gym
 
 def controller(observation):
+    GAIN = float(sys.argv[1])
     co, si, av  = observation
     action = si * -100 + av * -1
+    action *= GAIN
     print ("CONTROLLER obs:", co, si, av, "ACT:", action)
     return [action]
 
 
 env = gym.make("Pendulum-v1", render_mode="human")
-env._max_episode_steps=200
+env._max_episode_steps=500
 while True:
     observation, info = env.reset()
     co, si, av = observation
@@ -20,15 +22,18 @@ time.sleep(1)
 
 resets = 0
 rewards = 0
-for _ in range(200):
-    print (_, end=" ")
+for ii in range(300):
+    print (ii, end=" ")
     action = controller(observation)
+    # print (action)
+    if ii == 200:
+        action = [100]
     observation, reward, terminated, truncated, info = env.step(action)
     rewards += reward
-    #print (_, observation, reward, terminated, truncated, info)
+    #print (ii, observation, reward, terminated, truncated, info)
     if terminated or truncated:
         print ("*************************RESET*************************")
-        print (_, observation, "rewards:", rewards)
+        print (ii, observation, "rewards:", rewards)
         observation, info = env.reset()
         resets += 1
         rewards = 0
