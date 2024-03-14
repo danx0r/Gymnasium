2b0bd9b30886f5c7353c3942fab65ca88597b237
danx0r <danbmil99@gmail.com>
2024-03-13 19:06:09 -0700

    simpl

--- pend.py
+++ pend.py
@@ -7,37 +7,28 @@ def controller(observation):
     co, si, av  = observation
     action = si * -100 + av * -1
     action *= GAIN
-    print ("CONTROLLER obs:", co, si, av, "ACT:", action)
+    # print ("CONTROLLER obs:", co, si, av, "ACT:", action)
     return [action]
 
 
-env = gym.make("Pendulum-v1", render_mode="human")
-env._max_episode_steps=500
-while True:
-    observation, info = env.reset()
-    co, si, av = observation
-    if abs(si) < .2 and co > .9:
-        break
-time.sleep(1)
+# env = gym.make("Pendulum-v1", render_mode="human")
+env = gym.make("Pendulum-v1")
+env._max_episode_steps=200
+observation, info = env.reset()
 
-resets = 0
-rewards = 0
-for ii in range(300):
-    print (ii, end=" ")
+STEPS = 100
+error = 0
+for ii in range(STEPS):
     action = controller(observation)
     # print (action)
-    if ii == 200:
-        action = [100]
     observation, reward, terminated, truncated, info = env.step(action)
-    rewards += reward
-    #print (ii, observation, reward, terminated, truncated, info)
+    error += abs(observation[1])
+    # print (ii, observation, error)
     if terminated or truncated:
         print ("*************************RESET*************************")
-        print (ii, observation, "rewards:", rewards)
+        print (ii, observation)
         observation, info = env.reset()
-        resets += 1
-        rewards = 0
     # time.sleep(.02)
+error = error ** .5 / STEPS
+print ("ERROR:", error)
 env.close()
-
-print ("Golf score:", resets)
