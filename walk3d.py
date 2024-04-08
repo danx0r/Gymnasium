bff555f9d10ed1bac242a341f2f28245fabb0b13
danx0r <danbmil99@gmail.com>
2024-04-07 20:50:56 -0700

    integration is key

--- walk3d.py
+++ walk3d.py
@@ -10,21 +10,11 @@ import torch
 from torch import nn
 
 #
-# hip_r, knee_r, foot_r, hip_l, knee_l, foot_l
-# (mujoco calls them thigh, leg, foot)
-# right is tan, left is purple
-# hip: -1 extend (back bend) 1 contract (knee to nose)
-# knee: -1 contract (heel to butt; kneeling), 1 extend (locked knee)
-# foot: -1 extend (high heel/en pointe), 1 contract (walk on your heels)
+# See README.txt
 #
-# observation[0] = torso height (z of center)
-# observation[1] = torso angle
-# observation[2:8] = angle of hip_r, knee_r, foot_r, hip_l, knee_l, foot
-# observation[8] = torso x vel
-# observation[9] = torso z vel
-# observation[10] = torso ang vel
-# observation[11:16] = ang vel of hip_r, knee_r, foot_r, hip_l, knee_l, foot
-#
+
+NUM_SERVOS = 10
+
 class Servo:
     def __init__(self, P, D, name):
         self.P = P
@@ -38,50 +28,47 @@ class Servo:
     def update(self, pos, vel):
         perr = pos-self.target
         torque = perr * self.P + vel * self.D
-        # if self.name in ("foot_l", "hip_l"):
-        # print ("TORQUE:", self.name, pos, vel, perr, torque)
-        # sys.stdout.flush()
         return torque        
 
 class Controller:
-    # PDvals = [
-    #     (-7.0, -0.5),   #hip_r
-    #     (-3.0, -0.2),   #knee_r
-    #     (-1.0, -0.1),   #foot_r
-    #     (-7.0, -0.5),   #hip_l
-    #     (-3.0, -0.2),   #knee_l
-    #     (0, 0),   #foot_l
-    #     ]
-
-    GAIN = 1
+    PGAIN = 10
+    DGAIN = 1
     PDvals = [
-        (-.9*GAIN, -0.12*GAIN),   #knee_l
-        (-.9*GAIN, -0.084*GAIN),   #knee_l
-        (-.9*GAIN, -0.084*GAIN),   #knee_l
-        (-.9*GAIN, -0.12*GAIN),   #knee_l
-        (-.9*GAIN, -0.084*GAIN),   #knee_l
-        (-.9*GAIN, -0.084*GAIN),   #knee_l
+        (-PGAIN, -DGAIN),   #hip L1
+        (-PGAIN, -DGAIN),   #hip L2
+        (-PGAIN, -DGAIN),   #hip R1
+        (-PGAIN, -DGAIN),   #hip R2
+        (-PGAIN, -DGAIN),  #knee_L
+        (-PGAIN, -DGAIN),  #knee_R
+        (-PGAIN, -DGAIN),   #ankle L
+        (-PGAIN, -DGAIN),  #ankle_R
+        (-PGAIN, -DGAIN),  #foot L
+        (-PGAIN, -DGAIN),  #foot_R
         ]
 
     joints = {
-        'hip_r': 0,
-        'knee_r': 1,
-        'foot_r': 2,
-        'hip_l': 3,
+        'hip_l1': 0,
+        'hip_l2': 1,
+        'hip_r1': 2,
+        'hip_r2': 3,
         'knee_l': 4,
-        'foot_l': 5,
+        'knee_r': 5,
+        'ankle_l': 6,
+        'anlke_r': 7,
+        'foot_l': 8,
+        'foot_r': 9,
     }
 
     def __init__(self):
-        self.servos = [None] * 6
-        self.act = [0] * 6
-        for i in range(6):
+        self.servos = [None] * NUM_SERVOS
+        self.act = [0] * NUM_SERVOS
+        for i in range(NUM_SERVOS):
             self.servos[i] = Servo(self.PDvals[i][0], self.PDvals[i][1], list(self.joints.keys())[i])
     
     def update(self, obs):
-        for i in range(6):
-            pos = obs[2 + i]
-            vel = obs[11 + i]
+        for i in range(NUM_SERVOS):
+            pos = 0
+            vel = 0
             self.act[i] = self.servos[i].update(pos, vel)
         return self.act
 
@@ -96,34 +83,30 @@ class Controller:
             s.D *= adj
  
 def runn(env, steps, adjust=None):
-    speed = 0.032
-    hip_range = 0.4
-    hip_offset = 0.1
-    hip_l_phase = math.pi / 2
-    hip_r_phase = -math.pi / 2
-    knee_range = 0.56
-    knee_offset = 0.1
-    knee_l_phase = -math.pi / 2
-    knee_r_phase = math.pi / 2
-    foot_range = 0.25
-    foot_offset = .14
-    foot_l_phase = math.pi / 2
-    foot_r_phase = -math.pi / 2
+    # speed = 0.032
+    # hip_range = 0.4
+    # hip_offset = 0.1
+    # hip_l_phase = math.pi / 2
+    # hip_r_phase = -math.pi / 2
+    # knee_range = 0.56
+    # knee_offset = 0.1
+    # knee_l_phase = -math.pi / 2
+    # knee_r_phase = math.pi / 2
+    # foot_range = 0.25
+    # foot_offset = .14
+    # foot_l_phase = math.pi / 2
+    # foot_r_phase = -math.pi / 2
 
     controller = Controller()
-    controller.goto('foot_l', .03)
-    controller.goto('foot_r', .02)
-    controller.goto('hip_l', .25)
-    controller.goto('hip_r', .25)
+    # controller.goto('foot_l', .03)
+    # controller.goto('foot_r', .02)
+    # controller.goto('hip_l', .25)
+    # controller.goto('hip_r', .25)
     observation, info = env.reset()
-    testact = 1
+
     for ii in range(steps):
         print ("STEP:", ii)
-        if ii==200:
-            # controller.adjust_gain(adjust)
-            testact = -1
-        # action = controller.update(observation)
-        action = [1, testact, -1, -testact, -testact, testact, 1, -1, 1, -1]
+        action = controller.update(observation)
         observation, reward, terminated, truncated, info = env.step(action)
         if VERBOSE & 1:
             print (ii, "OBSERVATION:", observation[:5], "\nACTION:", action)
@@ -131,18 +114,18 @@ def runn(env, steps, adjust=None):
         if terminated or truncated:
             break
         
-        hip_l = math.sin(ii * speed + hip_l_phase) * hip_range + hip_offset
-        controller.goto('hip_l', hip_l)
-        hip_r = math.sin(ii * speed + hip_r_phase) * hip_range + hip_offset
-        controller.goto('hip_r', hip_r)
-        knee_l = math.sin(ii * speed + knee_l_phase) * knee_range + knee_offset
-        controller.goto('knee_l', knee_l)
-        knee_r = math.sin(ii * speed + knee_r_phase) * knee_range + knee_offset
-        controller.goto('knee_r', knee_r)
-        foot_l = math.sin(ii * speed + foot_l_phase) * foot_range + foot_offset
-        controller.goto('foot_l', foot_l)
-        foot_r = math.sin(ii * speed + foot_r_phase) * foot_range + foot_offset
-        controller.goto('foot_r', foot_r)
+        # hip_l = math.sin(ii * speed + hip_l_phase) * hip_range + hip_offset
+        # controller.goto('hip_l', hip_l)
+        # hip_r = math.sin(ii * speed + hip_r_phase) * hip_range + hip_offset
+        # controller.goto('hip_r', hip_r)
+        # knee_l = math.sin(ii * speed + knee_l_phase) * knee_range + knee_offset
+        # controller.goto('knee_l', knee_l)
+        # knee_r = math.sin(ii * speed + knee_r_phase) * knee_range + knee_offset
+        # controller.goto('knee_r', knee_r)
+        # foot_l = math.sin(ii * speed + foot_l_phase) * foot_range + foot_offset
+        # controller.goto('foot_l', foot_l)
+        # foot_r = math.sin(ii * speed + foot_r_phase) * foot_range + foot_offset
+        # controller.goto('foot_r', foot_r)
 
     return ii
 
